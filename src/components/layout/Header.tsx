0c8542f5761bb0f8e795f38b7eab0256c0f99be5
JuanPabloBallesteros1905 <juanpablo-1905@hotmail.com>
2025-08-14 21:32:56 -0500

    newChanges

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -33,7 +33,7 @@ export const Header: React.FC = () => {
               <Code className="w-5 h-5 text-white" />
             </div>
             <span className="text-xl font-bold text-gray-900 dark:text-white">
-              Portfolio
+              Jpdev
             </span>
           </motion.div>
 
